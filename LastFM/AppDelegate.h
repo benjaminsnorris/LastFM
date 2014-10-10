8b78217446159e889cfb7600503073fc4e7c174c
Ben Norris <ben@bsn.io>
2014-10-09 18:22:12 -0600

    Base project with AFNetworking

---
+++ LastFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LastFM
+//
+//  Created by Ben Norris on 10/9/14.
+//  Copyright (c) 2014 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LastFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LastFM
+//
+//  Created by Ben Norris on 10/9/14.
+//  Copyright (c) 2014 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
